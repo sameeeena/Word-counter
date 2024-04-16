e1cdf8c31b3ba7f24a88e95baddb1c330df54a93
sameeeena <162378926+sameeeena@users.noreply.github.com>
2024-04-17 03:19:19 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// import the 'inquirer' modulewhish is a command line interface.
+import inquirer from "inquirer";
+// declare a constant 'answers' and assign it to the result of inquirer.prompt function
+const answers = await inquirer.prompt([{
+        name: "sentence",
+        type: "input",
+        message: " Enter your sentence to count the word;"
+    }]);
+const words = answers.sentence.split(" ");
+//print the array of words to the console
+console.log(words);
+// print the word count of the sentence to the console
+console.log(`Your sentence word count is ${words.length}`);
